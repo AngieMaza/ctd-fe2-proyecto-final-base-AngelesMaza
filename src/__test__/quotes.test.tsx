ef41d2d1f745452369430a9e402f5fcf65e246da
Angeles Maza <angie.maza.89@gmail.com>
2022-12-05 22:59:59 -0300

    tests added

--- src/__test__/quotes.test.tsx
+++ src/__test__/quotes.test.tsx
@@ -1,24 +1,29 @@
 import Quote from "../features/quote/quote";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { render } from "../test-utils";
 
 describe ("quotes component", () => {
     it ("Should render a quote randomly", async() => {
         render(<Quote />);
-        const input = screen.getByPlaceholderText("Insert Quote Author name");
-        userEvent.type(input, "")
+        const button = screen.getByText("Get Quote randomly");
+        fireEvent.click(button);
         expect(await screen.findByText("Gah, stupid sexy Flanders!")).toBeInTheDocument();
     })
     it ("Should render a quote by writter", async() => {
         render(<Quote />);
         const input = screen.getByPlaceholderText("Insert Quote Author name");
-        userEvent.type(input, "Nelson")
+        userEvent.type(input, "Nelson");
+        const button =await screen.findByText("Get Quote");
+        fireEvent.click(button);
         expect(await screen.findByText("Shoplifting is a victimless crime, like punching someone in the dark.")).toBeInTheDocument();
     })
     it ("Should render a error message", async() => {
         render(<Quote />);
         const input = screen.getByPlaceholderText("Insert Quote Author name");
         userEvent.type(input, "123")
+        const button =await screen.findByText("Get Quote");
+        fireEvent.click(button);
         expect(await screen.findByText("Please enter a valid name")).toBeInTheDocument();
     })
 })
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -3,7 +3,7 @@ import { API_URL } from "../app/constants";
 
 export const handlers = [
     rest.get(API_URL, ( req , res , ctx ) => {
-        if (req.url.searchParams.get("character")==="Nelson") {
+        if (req.url.searchParams.get("character") === "Nelson") {
             return res (
                 ctx.json([
                     {
@@ -15,14 +15,15 @@ export const handlers = [
                 ])
             )
         } else {
-            ctx.json([
+            return res(ctx.json([
                 {
                     "quote": "Gah, stupid sexy Flanders!",
                     "character": "Homer Simpson",
                     "image": "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939",
                     "characterDirection": "Right"
                 }
-            ])
+            ]))
+            
         }
         
     })
